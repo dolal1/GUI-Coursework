3de3eb3bb8160fe60bf5e241a9dc5c344456faf0
David OH <holifeilddavid@gmail.com>
2021-12-30 16:04:39 +0300

    KMIS Packae Added

---
+++ src/com/ooad/kmis/Subjects.java
@@ -0,0 +1,9 @@
+package com.ooad.kmis;
+
+public final class Subjects {
+	public final static String math = "Mathematics";
+	public final static String english = "English";
+	public final static String science = "Science";
+	public final static String sst = "Social Studies";
+
+}
