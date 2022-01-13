2a61640c71506727491ecf25921b2cadc29cc2ed
TONNY BAWEMBYE <tonnybaw75@gmail.com>
2022-01-13 13:15:11 +0300

    Created executable file

--- src/com/ooad/kmis/teacher/ClassTimeTablePanel.java
+++ src/com/ooad/kmis/teacher/ClassTimeTablePanel.java
@@ -26,10 +26,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class ClassTimeTablePanel extends JPanel {
 
